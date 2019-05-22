d6e6caa84429ccf0423653b30f914442fd611e0d
Shashank Gupta <shashankgupta.sg01@gmail.com>
2019-05-22 22:23:04 +0530

    Bug fix : export env from config

--- config.js
+++ config.js
@@ -1,9 +1,11 @@
 // config.js
 const dotenv = require('dotenv');
 dotenv.config();
-module.exports.env = {
+var env = {
     client_secret : process.env.CLIENT_SECRET,
     client_id: process.env.CLIENT_ID,
     verification_token: process.env.VERIFICATION_TOKEN,
     bot_token: process.env.BOT_TOKEN
 };
+
+module.exports = env;
